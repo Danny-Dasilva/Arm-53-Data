5c777e40b8c94ac974228d24985817c5bf7ac315
Danny Dasilva <yahchayildasilva@gmail.com>
2021-02-22 08:16:32 -0500

    Update mount_sd.sh

--- mount_sd.sh
+++ mount_sd.sh
@@ -6,8 +6,8 @@ sudo mount /dev/mmcblk1 /home/mendel/sd_card
 
 cd /home/mendel/sd_card
 
-sudo fallocate -l 10G /home/mendel/sd_card
-sudo chmod 600 /home/mendel/sd_card
-sudo mkswap /home/mendel/sd_card
-sudo swapon /home/mendel/sd_card
+sudo fallocate -l 10G /home/mendel/sd_card/swapfile
+sudo chmod 600 /home/mendel/sd_card/swapfile
+sudo mkswap /home/mendel/sd_card/swapfile
+sudo swapon /home/mendel/sd_card/swapfile
 
